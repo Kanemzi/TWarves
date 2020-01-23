8586a221f77dfe5be1ab92109d1ac97d61235805
Simon Rozec <simonrozec44@gmail.com>
2020-01-23 16:23:09 +0100

    etat dig -> début du système de choix de veine

--- characters/dwarf/states/dig.gd
+++ characters/dwarf/states/dig.gd
@@ -5,9 +5,10 @@ var vein: Vein
 func physics_process(delta: float) -> void:
 	pass
 
+
 func enter(params := {}) -> void:
-	var vein_index: int = clamp(params.spot, 1, 3) - 1 
-	var veins = get_tree().get_nodes_in_group("vein")
+	var veins = _get_veins()
+	var vein_index: int = clamp(params.spot, 1, veins.size()) - 1 
 	vein = veins[vein_index]
 	if vein == null :
 		#TODO find nearest vein
@@ -17,10 +18,25 @@ func enter(params := {}) -> void:
 	owner.connect("target_reached", self, "_on_Dwarf_target_reached")
 	owner.animator.play("run")
 
+
 func exit() -> void:
 	owner.disconnect("target_reached", self, "_on_Dwarf_target_reached")
-	
+
+
+func _get_veins() -> Array:
+	return get_tree().get_nodes_in_group("vein")
+
+
+func _find_nearest_vein() -> Vein:
+	var veins = _get_veins()
+	var nearest = veins[0]
+	var distance = owner.position.distance_to(nearest.position)
+	for v in veins:
+		if owner.position.distance_to(v.position) < distance:
+			nearest = v
+	return nearest
+
+
 func _on_Dwarf_target_reached() -> void:
 	owner.animator.play("dig")
 	owner.sprite.set_direction(vein.position.x - owner.position.x)
-		
