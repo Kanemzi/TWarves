f01df5f0850d59660d3fe0140d6627636b065253
Simon Rozec <simonrozec44@gmail.com>
2020-02-17 10:46:02 +0100

    Limitation du nombre de connexions au serveur par TDL + gestion d'erreurs à la réception de messages TDL

--- twitch-extension/extension-twarves-panel/services/client-link/TDLServer.js
+++ twitch-extension/extension-twarves-panel/services/client-link/TDLServer.js
@@ -3,20 +3,27 @@
  * permettant de gérer les interactions entre le jeu et la base de données distante
  */
 
-const http = require('http')
 const ws = require('ws').Server
-
 require('dotenv').config()
-console.log(process.env.DB_NAME)
 
 const {Type, TDLMessage} = require('./TDLMessage')
 const handlers = require('./handlers')
 
-let client
+/*
+  référence vers le client actuellement connecté au serveur
+  il ne peut exister qu'un seul client connecté au serveur à la fois 
+*/
+let client = null
 let wsServer = new ws({port: 5700})
 
 wsServer.on('connection', (ws) => {
-	console.log('new connection')
+  console.log('new connection')
+  // si le serveur est déjà connecté à un client, on interdit les nouvelles connexions
+  if (client != null) {
+    console.log('new connection refused')
+    ws.close()
+    return
+  }
 	client = ws
 	
 	let message = new TDLMessage(Type.PLAYER_INFORMATION, {test: '123'})
@@ -25,21 +32,32 @@ wsServer.on('connection', (ws) => {
   ws.on('message', (message) => {
     handleMessage(message)
   })
+  
+  ws.onclose = () => {
+    console.log("closed");
+    client = null
+  };
 })
 
+
 const handleMessage = message => {
-  let json = JSON.parse(message)
+  let json
+  try {
+    json = JSON.parse(message)
+  } catch (e) {
+    return console.error(e)
+  }
+
   if (handlers[json.type] !== undefined)
     handlers[json.type](json.message)
 }
 
+
 /**
- * @param {WebSocket} socket 
- * @param {String} type 
  * @param {String} message 
  */
 const sendMessage = (message) => {
-  if (client !== undefined)
+  if (client !== null)
 		client.send(message)
 }
 
