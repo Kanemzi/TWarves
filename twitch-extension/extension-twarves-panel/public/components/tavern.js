db8636ce85dd668dae3325e513a22bc03195f6e7
SimonROZEC <simonrozec44@gmail.com>
2020-02-23 15:13:20 +0100

    Fonctionnement on/off du panel de la taverne

--- twitch-extension/extension-twarves-panel/public/components/tavern.js
+++ twitch-extension/extension-twarves-panel/public/components/tavern.js
@@ -1,13 +1,21 @@
 Vue.component('tavern', {
 	template: `
-<section id="tavern">
-	<div class="tavern-button">Taverne</div>
+<section id="tavern" :class="{ opened : panelOpened }">
+	<div class="tavern-button" @click="onButtonClick">Taverne</div>
 	<div class="tavern-panel">
+		Rien à voir ici pour le moment !
 	</div>
 </section>
 	`,
 	data: () => {
 			return {
+				panelOpened: false
 			}
+	},
+	methods: {
+		onButtonClick: function (ev) {
+			this.panelOpened = !this.panelOpened
+			console.log('clicked')
+		}
 	}
 })
